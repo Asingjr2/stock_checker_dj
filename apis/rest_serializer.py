157c9f76f8e28556752c23672ebb3dd6cc483dbc
asingjr2 <asingjr2@gmail.com>
2018-08-04 19:58:38 -0400

    updated template and views.  Added delete option and altered base methods

--- apis/rest_serializer.py
+++ apis/rest_serializer.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers 
 
 from stocks.models import Stock
@@ -6,7 +8,13 @@ from stocks.models import Stock
 class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model= Stock
-        fields = ["ticker_symbol", "pk", "user", "s_open", "s_close", "s_high", "s_low"]
+        fields = ["ticker_symbol", "id", "user", "s_open", "s_close", "s_high", "s_low"]
 
     def get_url(self, obj):
         return obj.get_url()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]
--- apis/rest_urls.py
+++ apis/rest_urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .rest_views import CreateAPIView, DeleteAPIView
+from .rest_views import CreateAPIView, ListAPIView, DeleteAPIView, UserDeleteAPIView
 
 
 app_name="rest"
 urlpatterns = [
     path('create/', CreateAPIView.as_view(), name="create"),
-    path('delete/<uuid:pk>/', DeleteAPIView.as_view(), name="delete"),
+    path("list/", ListAPIView.as_view(), name="list"),
+    path('delete/<uuid:pk>', DeleteAPIView.as_view(), name="delete"),
+    path("userdelete/<int:pk>", UserDeleteAPIView.as_view(), name="userdelete")
 ]
