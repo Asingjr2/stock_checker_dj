25f469d1a5e69eda8a9ce42cf63d5952e1479fa2
asingjr2 <asingjr2@gmail.com>
2018-08-08 20:54:20 -0400

    updated settings to account for allauth

--- stock_check/settings.py
+++ stock_check/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # Heroku setting for database
         'CONN_MAX_AGE': 500
     }
 }
